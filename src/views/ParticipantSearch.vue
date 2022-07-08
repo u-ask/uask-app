495adc8190d75d6b39f42419f45d9da36d9faa3d
YDarma <yves.darmaillac@arone.com>
2022-07-08 10:51:36 +0200

    not needed anymore

--- src/views/ParticipantSearch.vue
+++ src/views/ParticipantSearch.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row v-if="supportedSurvey">
+  <v-row>
     <v-col cols="12" :class="{ 'px-0': mobile }">
       <participant-filter @filterchanged="filter = $event"></participant-filter>
     </v-col>
@@ -10,17 +10,6 @@
       <participant-list :filter="filter"></participant-list>
     </v-col>
   </v-row>
-  <div v-else>
-    <v-card class="mx-auto" max-width="50em">
-      <v-alert border="top" class="not-supported" color="red lighten-2" dark>
-        {{ $t("compatibility") }}
-        <br />
-        <v-btn @click="$router.push('/')" text color="secondary">
-          {{ $t("home") }}
-        </v-btn>
-      </v-alert>
-    </v-card>
-  </div>
 </template>
 
 <script>
@@ -49,26 +38,6 @@ export default {
     },
     idSampleFilter() {
       return this.filter.fixed?.["@SAMPLE"];
-    },
-    supportedSurvey() {
-      return (
-        !!this.currentSurvey.mainWorkflow &&
-        !!this.currentSurvey.mainWorkflow.info
-      );
-    }
-  },
-  i18n: {
-    messages: {
-      en: {
-        home: "Go to home page",
-        compatibility:
-          "This application requires surveys that have a synthesis page.\nPlease fix it with the API or create a new one from the application home page."
-      },
-      fr: {
-        home: "Aller sur la page d'accueil",
-        compatibility:
-          "Cette application nécessite que les études disposent d'une page de synthèse. Veuillez corriger cela en utilisant l'API ou en créer une nouvelle depuis la page d'accueil de l'application."
-      }
     }
   }
 };
